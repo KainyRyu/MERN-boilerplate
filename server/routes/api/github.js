42735cf95b72267353aeb299bd2c5d81066214ac
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-20 15:13:01 +0000

    Step 2

--- server/routes/api/github.js
+++ server/routes/api/github.js
@@ -1,9 +1,11 @@
+const request = require('superagent');
+
 module.exports = (app) => {
     app.get('/user/signin/callback', (req, res, next) => {
         const { query } = req;
         // ?code=29384791287
-        const { code } = code;
-        console.log('code', code);
+        const { code } = query;
+        // console.log('code', code);
 
         if (!code) {
             return res.send({
@@ -13,7 +15,30 @@ module.exports = (app) => {
         }
 
         //POST
+        request
+            .post('https://github.com/login/oauth/access_token')
+            .send({ 
+                client_id: '690baf8f3ec899363b4f', 
+                client_secret: '6c78de167fc0f5602cee8997852ac31b2365638e',
+                code: code
+             })
+            .set('Accept', 'application/json')
+            .then(result => {
+                const data = result.body;
 
-        console.log('code', code);
+                res.send(data);
+        });
+
+        app.get('/user/', (req, res, next) => {
+            const accessToken = '159276211d2254898f672cf9c3d76edab5400696';
+            //the idea is you get it from backend
+            
+            request
+                .get('https://api.github.com/user')
+                .set('Authorization', 'token ' + accessToken)
+                .then(result => {
+                    res.send(result.body);
+                });
+        });
     });
 }
